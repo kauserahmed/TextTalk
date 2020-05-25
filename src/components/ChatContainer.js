b385043883d21c4eeff6989be54fbd7000c431f8
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-05-25 01:43:34 -0400

    add reset chat and addchat for the chat funcation, so the new chat can be enter

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -11,6 +11,30 @@ export default class ChatContainer extends Component {
     }
 }
 
+componentDidMount() {
+    const { socket } = this.props
+    socket.emit(COMMUNITY_CHAT, this.resetChat)
+}
+
+resetChat = (chat)=>{
+    return this.addChat(chat, true)
+}
+
+addChat = (chat, reset)=>{
+    const { socket } = this.props
+    const { chats } = this.state
+
+    const newChats = reset ? [chat] : [...chats, chat]
+    this.setState({chats:newChats, activeChat:reset ? chat : this.state.activeChat})
+
+    const messageEvent = `${MESSAGE_RECIEVED}-${chat.id}`
+    const typingEvent = `${TYPING}-${chat.id}`
+
+    socket.on(typingEvent, this.updateTypingInChat(chat.id))
+    socket.on(messageEvent, this.addMessageToChat(chat.id))
+}
+
+
 render() {
     const { user, logout } = this.props
     const { chats, activeChat } = this.state
