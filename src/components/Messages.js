f6b323089724da220a58b704a262ff056d6cd7af
saiful isalm <saiful_topu89@yahoo.com>
2020-05-24 23:37:53 -0400

    add continder for message

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,3 +1,21 @@
 import React, { Component } from 'react';
 
+export default class Messages extends Component {
+	constructor(props) {
+      super(props);
+    }
+}
+render() {
+    const { messages, user, typingUsers } = this.props
+    return (
+        <div ref='container'
+            className="thread-container">
+            <div className="thread">
+                {
 
+                }
+                
+                </div>
+
+
+</div>
