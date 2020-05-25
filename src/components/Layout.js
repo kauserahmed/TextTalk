4f0de0d5fd3d34d99a29d620190be46946b9bdd3
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-05-25 01:34:14 -0400

    initilized default constractor for chat container and render it.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import io from 'socket.io-client'
 import { USER_CONNECTED, LOGOUT } from '../Events'
 import LoginForm from './LoginForm'
-
+import ChatContainer from './ChatContainer'
 
 const socketUrl = "http://localhost:3231"
 export default class Layout extends Component {
