c118786874ced798d675daa3de72d4188a3c3c2d
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-05-24 21:25:41 -0400

    create a chat and message for server (socket.io)

--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -1,6 +1,24 @@
 const io = require('./index.js').io
 const { createUser, createMessage, createChat } = require('../Create')
+const { VERIFY_USER, USER_CONNECTED, USER_DISCONNECTED, 
+    LOGOUT, COMMUNITY_CHAT, MESSAGE_RECIEVED, MESSAGE_SENT,
+    TYPING  } = require('../Events')
+
+let connectedUsers = { }
+
+let communityChat = createChat()
 
 module.exports = function(socket){
     console.log("Socket Id:" + socket.id);
+    let sendMessageToChatFromUser;
+    let sendTypingFromUser;
+    
+    socket.on(VERIFY_USER, (nickname, callback)=>{
+		if(isUser(connectedUsers, nickname)){
+			callback({ isUser:true, user:null })
+		}else{
+			callback({ isUser:false, user:createUser({name:nickname})})
+        }
+    })
+
 }
