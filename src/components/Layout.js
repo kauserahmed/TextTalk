cdbbf77e4082a0caba6f609f614e7544e63f4244
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-05-24 21:50:01 -0400

    connect and initilized the user into socket.io

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,34 @@
 import React, { Component} from 'react';
+import io from 'socket.io-client'
+import { USER_CONNECTED, LOGOUT } from '../Events'
+import LoginForm from './LoginForm'
 
+
+const socketUrl = "http://localhost:3231"
 export default class Layout extends Component {
+	constructor(props) {
+		super(props);
+	  
+		this.state = {
+			socket:null,
+			user:null
+		};
+	  }
+
+	  componentWillMount() {
+		this.initSocket()
+	}
+
+	initSocket = ()=>{
+		const socket = io(socketUrl)
+
+		socket.on('connect', ()=>{
+			console.log("Connected");
+		})
+		
+		this.setState({socket})
+	}
+
     render() {
 		const { title } = this.props
 		return (
